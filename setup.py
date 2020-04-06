f9132b359fc9a234c561b26fa8c4c54d6aff7532
Ben Johnson <bkj.322@gmail.com>
2020-04-06 14:36:59 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env/python
+
+import exline
+from setuptools import setup, find_packages
+
+VERSION = exline.__version__
+
+setup(
+    name='amdim',
+    author='Ben Johnson',
+    author_email='ben@canfield.io',
+    classifiers=[],
+    description='amdim',
+    keywords=['amdim'],
+    license='ALV2',
+    packages=find_packages(),
+    version=VERSION
+)
